552e0f587d5590f0fd152285f31e87223ff2ab41
Elaine Matos <enimatos@live.com>
2023-08-24 00:08:01 -0300

    atualizações

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -1,15 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
     
     public static void main(String[] args) {
+        //criando objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        String nome = args[0];
-        String sobrenome = args[1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.print("Digite seu sobrenome: ");
+        String sobrenome = scanner.next();
+
+        System.out.print("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.print("Digite sua altura: ");
+        double altura = scanner.nextDouble();
 
         System.out.println("Olá, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " idade");
-        System.out.println("Minha altura é " + altura);
+        System.out.println("Minha altura é " + altura + "cm.");
 
     }
 }
