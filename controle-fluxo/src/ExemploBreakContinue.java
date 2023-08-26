49a00f0af1555e1266487a4fbc22c490e0304e91
Elaine Matos <enimatos@live.com>
2023-08-26 18:34:25 -0300

    atualização das classes

---
+++ controle-fluxo/src/ExemploBreakContinue.java
@@ -0,0 +1,10 @@
+public class ExemploBreakContinue {
+    public static void main(String[] args) {
+        
+        for(int num = 1; num <=5; num++){
+            if(num==3)
+                continue;
+            System.out.println(num);
+        }
+    }
+}
