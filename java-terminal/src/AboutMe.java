afbbf2bf1e8a16697f33b5b8abd259c8fcdfce18
Elaine Matos <enimatos@live.com>
2023-08-27 22:50:07 -0300

    exercico de excecoes

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -1,9 +1,11 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
     
     public static void main(String[] args) {
+        try{
         //criando objeto scanner
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
@@ -22,6 +24,11 @@ public class AboutMe {
         System.out.println("Olá, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " idade");
         System.out.println("Minha altura é " + altura + "cm.");
+        scanner.close();
+        }
+        catch (InputMismatchException e){
+            System.out.println("Os campos idade e altura precisam ser numéricos\n");
+        }
 
     }
 }
